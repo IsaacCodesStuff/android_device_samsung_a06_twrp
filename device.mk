7d12c5ed29cee827c27fa460f7da670fab2f97eb
Isaac <isaacvillamante225@gmail.com>
2026-01-09 19:06:37 +0800

    Remove patches folder and hope it works

---
+++ device.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/samsung/a06
+
+# Copy the files
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a06/recovery/root,recovery/root)
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
