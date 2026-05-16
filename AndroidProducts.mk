ab5523e81849afb7819d8f45e55f629ba76f8800
Isaac <isaacvillamante225@gmail.com>
2026-05-16 16:18:14 +0800

    a06: Fix some problems and trim more stuff

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,9 +1,2 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_a06.mk
--- twrp_a06.mk
+++ twrp_a06.mk
@@ -1,14 +1,8 @@
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit from a06 device
 $(call inherit-product, device/samsung/a06/device.mk)
 
 PRODUCT_DEVICE := a06
 PRODUCT_NAME := twrp_a06
 PRODUCT_MODEL := SM-A065F
-
-PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 BUILD_FINGERPRINT := samsung/a06xx/a06:15/AP3A.240905.015.A2/A065FXXS7BYJ1:user/release-keys
